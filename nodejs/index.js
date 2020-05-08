865e1304af986227b11be8af6f41327869a49be5
PuRple <kiyun1996@naver.com>
2020-05-08 22:29:45 +0900

    update 'index.js' file

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', function(req, res, next) {
+	res.sendFile(__dirname + '/index.html');
+});
 
 server.listen(8080, function() {
 	console.log('--- STANDING BY ---');
